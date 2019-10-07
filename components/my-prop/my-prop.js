c06397f0823aff462edcba6d23859088f8e904c5
Fjh919598225 <Fjh919598225@163.com>
2019-10-07 23:27:37 +0800

    自定义组件

---
+++ components/my-prop/my-prop.js
@@ -0,0 +1,18 @@
+// components/my-prop/my-prop.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:"我是默认值",
+      observer:function(newVal,oldVal){
+        console.log(newVal,oldVal);
+      }
+    }
+  },
+  externalClasses: ['titleclass']
+
+
+})
